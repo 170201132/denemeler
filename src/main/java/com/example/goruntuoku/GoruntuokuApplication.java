a322a50b02bb480095caf1bf15a96427813927a0
Saffet Bayramoğlu <170201132@kocaeli.edu.tr>
2020-04-17 15:34:49 +0300

    proje

---
+++ src/main/java/com/example/goruntuoku/GoruntuokuApplication.java
@@ -0,0 +1,22 @@
+package com.example.goruntuoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+
+public class GoruntuokuApplication {
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoruntuokuApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/example/goruntuoku/GoruntuokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goruntuoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoruntuokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
